ad7795130c519313f41bcb76a180aa2f5d31f9c1
zyang96 <zhongtao@gatech.edu>
2016-11-28 12:11:40 -0500

    unordered_map upper and lower bound

--- unordered_map/unordered_map.cpp
+++ unordered_map/unordered_map.cpp
@@ -18,11 +18,39 @@ int main()
 	cout << container.begin()->first << endl;
 	
 	//============without using insertion==========
+	
 	unordered_map<int,char> map;
 	map[1] = 's';
 	map[2] = 'a';
 	map[3] = 'd';
 	cout << map[1] << endl;
+	
+	
+	cout << "===================lower bound===================" << endl;
+	std::map<char,int> mymap;
+	std::map<char,int>::iterator itlow,itup;
+
+	mymap['a']=20;
+	mymap['b']=40;
+	mymap['c']=60;
+	mymap['d']=70;
+	mymap['z']=80;
+	mymap['e']=100;
+	
+	// comparating the key not the value
+	itlow=mymap.lower_bound ('b');  // itlow points to b
+	itup=mymap.upper_bound ('c');   // itup points to d (not !c)
+	cout << "itup points to d not c's value, itup->second = mymap['d'] =" << itup->second << endl;
+	// if mymap.upper_bound('z'), it will points to mymap.end();
+	if (mymap.upper_bound('z') == mymap.end()) cout << "it cannot find a upper bound that is larger than mymap['z'] = 80" << endl;
+
+	mymap.erase(itlow,itup);        // erases [itlow,itup) // exclude itup
+
+	// print content:
+	for (std::map<char,int>::iterator it=mymap.begin(); it!=mymap.end(); ++it)
+	std::cout << it->first << " => " << it->second << '\n';
+
+	return 0;
 	
 }
 
